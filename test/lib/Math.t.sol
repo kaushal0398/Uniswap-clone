4018b4987574ded20d15311273f2fafc27fdb828
KaushalPanpaliya <83158302+KaushalPanpaliya@users.noreply.github.com>
2024-05-29 17:35:50 +0530

    Update Math.t.sol

--- test/lib/Math.t.sol
+++ test/lib/Math.t.sol
@@ -26,23 +26,4 @@ contract MathTest is Test {
         assertEq(amount1, 4999.187247111820044641 ether);
     }
 
-    function testCalcAmount0DeltaNegative() public {
-        int256 amount0 = Math.calcAmount0Delta(
-            TickMath.getSqrtRatioAtTick(85176),
-            TickMath.getSqrtRatioAtTick(86129),
-            int128(-1517882343751509868544)
-        );
-
-        assertEq(amount0, -0.998833192822975408 ether);
-    }
-
-    function testCalcAmount1DeltaNegative() public {
-        int256 amount1 = Math.calcAmount1Delta(
-            TickMath.getSqrtRatioAtTick(84222),
-            TickMath.getSqrtRatioAtTick(85176),
-            int128(-1517882343751509868544)
-        );
-
-        assertEq(amount1, -4999.187247111820044640 ether);
-    }
-}
+    
