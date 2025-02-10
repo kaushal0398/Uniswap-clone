91138dd48c3c4e182079a6595b9e126fe22f7447
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2025-02-10 23:48:37 +0530

    Update UniswapV3Factory.sol

--- src/UniswapV3Factory.sol
+++ src/UniswapV3Factory.sol
@@ -5,8 +5,7 @@ import "./interfaces/IUniswapV3PoolDeployer.sol";
 import "./UniswapV3Pool.sol";
 
 contract UniswapV3Factory is IUniswapV3PoolDeployer {
-    error PoolAlreadyExists();
-    error ZeroAddressNotAllowed();
+    
     error TokensMustBeDifferent();
     error UnsupportedFee();
 
