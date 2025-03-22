5689943990a9ab9bbd010900ed9c90d731132d5d
Kaushal <83158302+kaushal0398@users.noreply.github.com>
2025-03-22 23:55:01 +0530

    Update UniswapV3Factory.sol

--- src/UniswapV3Factory.sol
+++ src/UniswapV3Factory.sol
@@ -5,7 +5,7 @@ import "./interfaces/
 IUniswapV3PoolDeployer.sol";
 import "./UniswapV3Pool.sol";
 
-contract UniswapV3Factory is IUniswapV3PoolDeployer {
+ IUniswapV3PoolDeployer {
     error PoolAlreadyExists();
     error ZeroAddressNotAllowed();
     error TokensMustBeDifferent();
